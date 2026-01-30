769c99a98dc0bcf77f3b3fe2df4738a27313ef10
QuasarHusky <quasarhusky@gmail.com>
2026-01-30 22:31:36 +0000

    Remove built-in apps

---
+++ apps/test/__init__.py
@@ -0,0 +1,15 @@
+import os
+import sys
+# from badgeware import run
+
+sys.path.insert(0, "/system/apps/test")
+os.chdir("/system/apps/menu")
+
+def init():
+    pass
+
+def update():
+    pass
+
+def on_exit():
+    pass
