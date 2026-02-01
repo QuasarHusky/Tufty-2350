07a61933988b6472933db2973812bc0d93d0a21b
QuasarHusky <quasarhusky@gmail.com>
2026-02-01 03:41:23 +0000

    Add Vinyl Scratch to dance app

--- apps/dance/characters/__init__.py
+++ apps/dance/characters/__init__.py
@@ -1,5 +1,7 @@
 import characters.paperbark as paperbark
+import characters.vinyl_scratch as vinyl_scratch
 
 characters = [
     paperbark,
+    vinyl_scratch,
 ]
---
+++ apps/dance/characters/vinyl_scratch/__init__.py
@@ -0,0 +1,8 @@
+name = "Vinyl Scratch"
+
+animations = [
+    {
+        "path": "characters/vinyl_scratch/dance_2.png",
+        "frames": 27,
+    },
+]
