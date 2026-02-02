50b732f0471bcb7b40d68aaed08fdfd73a7d2d1c
QuasarHusky <quasarhusky@gmail.com>
2026-02-02 05:12:03 +0000

    More dance animations for Vinyl Scratch

--- apps/dance/characters/vinyl_scratch/__init__.py
+++ apps/dance/characters/vinyl_scratch/__init__.py
@@ -2,8 +2,23 @@ name = "Vinyl Scratch"
 
 animations = [
     {
+        "path": "characters/vinyl_scratch/dance_8.png",
+        "frames": 31,
+        "auto_cycle": True,
+    },
+    {
+        "path": "characters/vinyl_scratch/dance_7.png",
+        "frames": 28,
+        "auto_cycle": True,
+    },
+    {
         "path": "characters/vinyl_scratch/dance_2.png",
         "frames": 27,
+        "auto_cycle": True,
+    },
+    {
+        "path": "characters/vinyl_scratch/jump.png",
+        "frames": 26,
         "auto_cycle": True,
     },
 ]
