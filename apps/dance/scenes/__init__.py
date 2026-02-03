3beca634febcc755d7fe87a15f36a746912b006a
QuasarHusky <quasarhusky@gmail.com>
2026-02-03 03:25:08 +0000

    Remove "stage" scene from dance app

--- apps/dance/scenes/__init__.py
+++ apps/dance/scenes/__init__.py
@@ -1,11 +1,9 @@
 import scenes.visualiser as visualiser
-import scenes.stage as stage
 import scenes.space as space
 import scenes.void as void
 
 scenes = [
-    space,
-    stage,
     visualiser,
+    space,
     void,
 ]
