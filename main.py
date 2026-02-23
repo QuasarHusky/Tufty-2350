209b335344dfb31534f00c81970afb6c25dd98e2
QuasarHusky <quasarhusky@gmail.com>
2026-02-23 06:12:31 +0000

    Re-launch previous app when woken

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import sys
 import os
-from badgeware import fatal_error, display, DEFAULT_FONT, State
+from badgeware import fatal_error, display, DEFAULT_FONT, State, file_exists
 import machine
 import gc
 
 system_state = {
     "backlight": 0.5,
+    "launch_app": None,
 }
 State.load("quasar.system", system_state)
 
@@ -61,18 +62,33 @@ def run(app):
     except Exception as e:
         fatal_error("App Error", e)
 
-try:
-    sys.path.insert(0, "/system/launcher")
-    launcher = __import__("/system/launcher")
-except Exception as e:  # noqa: BLE001
-    fatal_error("System Error", e)
+def quit_to_launcher(pin):
+    system_state["launch_app"] = None
+    State.save("quasar.system", system_state)
+    
+    while not pin.value():
+        pass
+    
+    machine.reset()
 
-app = run(launcher)
 
-if sys.path[0].startswith("/system/launcher"):
-    sys.path.pop(0)
 
-del launcher
+app = system_state["launch_app"]
+
+if app == None:
+    try:
+        sys.path.insert(0, "/system/launcher")
+        launcher = __import__("/system/launcher")
+    except Exception as e:  # noqa: BLE001
+        fatal_error("System Error", e)
+    
+    app = run(launcher)
+
+    if sys.path[0].startswith("/system/launcher"):
+        sys.path.pop(0)
+    
+    del launcher
+
 
 for key, _module in sys.modules.items():
     if key not in standard_modules:
@@ -83,15 +99,26 @@ gc.collect()
 if app is None:
     fatal_error("System Error", "Launcher did not provide an app to run")
 
+system_state["launch_app"] = app
+State.save("quasar.system", system_state)
+
+io.poll()
+io.poll()
 while io.held:
     io.poll()
+
+machine.Pin.board.BUTTON_HOME.irq(
+    trigger=machine.Pin.IRQ_FALLING, handler=quit_to_launcher
+)
 
 sys.path.insert(0, app)
 
 try:
     os.chdir(app)
     running_app = __import__(app)
-except Exception as e:  # noqa: BLE001
+except Exception as e:
+    system_state["launch_app"] = None
+    State.save("quasar.system", system_state)
     fatal_error("System Error", e)
 
 run(running_app)
