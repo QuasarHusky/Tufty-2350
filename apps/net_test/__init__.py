1fd16288f29b1eb477881cd31c7880a2b45cea29
QuasarHusky <quasarhusky@gmail.com>
2026-02-26 00:43:03 +0000

    net_test: Added app to test networking features

---
+++ apps/net_test/__init__.py
@@ -0,0 +1,47 @@
+from badgeware import display
+import wifi_patched as wifi
+import requests
+import os
+
+def init():
+    global download
+
+    show_message("Starting...")
+    wifi.connect()
+
+    status, message = None, ""
+
+    while status != 3:
+        io.poll()
+        wifi.tick()
+        status, message = wifi.status()
+        show_message(f"[{status}] {message}")
+    
+    show_message("Downloading image...")
+    res = requests.get("https://quasar.dog/absolute-fluttershy.png", stream=True)
+    
+    show_message("Saving image...")
+    with open("/state/download.png", "wb") as file:
+        file.write(res.content)
+
+    download = image.load("/state/download.png")
+    os.remove("/state/download.png")
+
+    screen.clear()
+    screen.blit(download, vec2(0, 0))
+
+    display.update(screen.width == 320)
+
+def update():
+    pass
+
+def show_message(text):
+    screen.pen = color.rgb(0, 0, 0)
+    screen.clear()
+
+    text_w, text_h = screen.measure_text(text)
+
+    screen.pen = color.rgb(255, 255, 255)
+    screen.text(text, (screen.width - text_w) / 2, (screen.height - text_h) / 2)
+
+    display.update(screen.width == 320)
